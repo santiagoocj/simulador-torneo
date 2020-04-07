0b8ce06b6c708cf832b2a5d6b3d857f0f2298c08
santiago ocampo jimenez <santiagocampo163@gmail.com>
2020-04-07 16:51:56 -0500

    se crea el repositorio y se sube el proyecto base

---
+++ src/main/java/edu/torneo/simuladortorneo/SimuladorTorneoApplication.java
@@ -0,0 +1,13 @@
+package edu.torneo.simuladortorneo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorTorneoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorTorneoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/torneo/simuladortorneo/SimuladorTorneoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.torneo.simuladortorneo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorTorneoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
