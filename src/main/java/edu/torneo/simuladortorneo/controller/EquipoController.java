37c75a9422085a40911eceaa84284a26f39cc3dc
santiago ocampo jimenez <santiagocampo163@gmail.com>
2020-05-28 16:53:14 -0500

    se agrega la anotación crossOrigin que permite que permite que el front desarrollado en angular pueda haceder al dominio y consumir los servicios

--- src/main/java/edu/torneo/simuladortorneo/controller/EquipoController.java
+++ src/main/java/edu/torneo/simuladortorneo/controller/EquipoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController("EquipoController")
 @RequestMapping("/equipo")
 public class EquipoController {
@@ -19,6 +20,7 @@ public class EquipoController {
     public String registrarEquipo(@RequestBody EquipoCommand equipoCommand) throws BusinessExecption {
         return equipoService.registrarEquipo(equipoCommand);
     }
+
 
     @GetMapping("/listado")
     public List<EquipoCommand> listadoEquipos() throws BusinessExecption{
